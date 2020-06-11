321142e014b35578071a7461ed801c4a083c61e8
FelipeDecome <felipe.decome@hotmail.com>
2020-06-11 17:26:00 -0300

    roman2dec - done

--- projects/roman2dec/script.js
+++ projects/roman2dec/script.js
@@ -3,38 +3,87 @@ const $ = document.querySelector.bind(document);
 const inpRom = $('[name=roman]');
 const inpDec = $('[name=decimal]');
 
-const container = $('#errorContainer');
+const errContainer = $('.errorContainer');
 
+const roman = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM', 'I', 'V', 'X', 'L', 'C', 'D', 'M'];
+const decimal = [4, 9, 40, 90, 400, 900, 1, 5, 10, 50, 100, 500, 1000];
+
+inpRom.addEventListener('input', handleDecimalInput);
 inpRom.addEventListener('keypress', handleRomanInput);
+
+function parseRoman(string) {
+
+    let parsedArray = [];
+    let sum = 0;
+
+    roman.forEach((r, i) => {
+
+        let parsedRoman = string.match(new RegExp(r, 'g'));
+        string = string.replace(new RegExp(r, 'g'), '');
+
+        parsedArray[i] = parsedRoman;
+    });
+
+    parsedArray.forEach((n, i) => {
+
+        if (n != null) {
+
+            sum += n.length * decimal[i];
+        }
+    });
+
+    return sum;
+}
 
 function handleRomanInput(evt) {
 
     evt.preventDefault();
 
-    if (/\b[10]+\b/.test(evt.key)) {
+    let newNumber = evt.key.toUpperCase();
 
-        evt.target.value += evt.key;
-        handleBinaryInputChange();
+    if (roman.includes(newNumber)) {
+
+        evt.target.value += verifyRoman(newNumber);
     } else {
-        handleErrorMessage("Digite apenas '0' ou '1'");
+
+        handleErrorMessage("Digite apenas números romanos 'I, V, X, L, C, D e M'");
     }
+
+    handleDecimalInput(evt.target.value);
 }
 
-function handleBinaryInputChange(evt) {
+function verifyRoman(value) {
 
-    inpDec.value = parseInt(inpBin.value, 2);
+    let string = inpRom.value;
+
+    // ^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$
+    if (!/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(string + value)) {
+
+        handleErrorMessage('Verifique se o formato está correto');
+        return '';
+    }
+
+    return value;
+}
+
+function handleDecimalInput() {
+
+    inpDec.value = parseRoman(inpRom.value);
 }
 
 function handleErrorMessage(errMsg) {
 
     if (errMsg != '') {
 
-        container.innerText = errMsg;
-        container.style.display = 'block';
+        let msg = document.createElement('span');
+        msg.classList.add('errorMsg');
+
+        msg.innerText = errMsg;
+
+        errContainer.append(msg);
 
         setTimeout(() => {
-            container.innerText = '';
-            container.style.display = 'none';
-        }, 2000);
+            msg.remove();
+        }, 3000);
     }
 }
